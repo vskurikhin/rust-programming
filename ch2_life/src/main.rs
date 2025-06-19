b750122d1c703dbc1b8829f51c37e13037ac0ce7
Victor N. Skurikhin <VSkurikhin@GMail.Com>
2025-06-19 20:54:25 +0300

    formatting ch2_life/src/main.rs

--- ch2_life/src/main.rs
+++ ch2_life/src/main.rs
@@ -1,8 +1,8 @@
 extern crate rand;
 extern crate termion;
-use std::{env, thread, time};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::{env, thread, time};
 use termion::clear;
 use termion::color;
 
@@ -27,31 +27,41 @@ fn main() {
         world = populate_from_file(filename);
     }
 
-    println!("Population at generation {} is {}", generations, census(world));
+    println!(
+        "Population at generation {} is {}",
+        generations,
+        census(world)
+    );
     for _gens in 0..100 {
         let temp = generation(world);
         world = temp;
         generations += 1;
         println!("{}", clear::All);
         displayworld(world);
-        println!("{blue}Population at generation {g} is {c}", blue = color::Fg(color::Blue), g = generations, c = census(world));
-        thread::sleep(time::Duration::from_secs(2));
+        println!(
+            "{blue}Population at generation {g} is {c}",
+            blue = color::Fg(color::Blue),
+            g = generations,
+            c = census(world)
+        );
+        thread::sleep(time::Duration::from_millis(500));
     }
-
 }
 
-fn populate_from_file(filename: String) -> [[u8; 75]; 75] 
-{
+fn populate_from_file(filename: String) -> [[u8; 75]; 75] {
     let mut newworld = [[0u8; 75]; 75];
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
-    let mut pairs:  Vec<(usize, usize)> = Vec::new();
+    let mut pairs: Vec<(usize, usize)> = Vec::new();
     for (index, line) in reader.lines().enumerate() {
         let l = line.unwrap();
         let mut words = l.split_whitespace();
         let left = words.next().unwrap();
         let right = words.next().unwrap();
-        pairs.push((left.parse::<usize>().unwrap(), right.parse::<usize>().unwrap()));
+        pairs.push((
+            left.parse::<usize>().unwrap(),
+            right.parse::<usize>().unwrap(),
+        ));
     }
 
     for i in 0..74 {
@@ -60,22 +70,18 @@ fn populate_from_file(filename: String) -> [[u8; 75]; 75]
         }
     }
 
-    for (x,y) in pairs {
+    for (x, y) in pairs {
         newworld[x][y] = 1;
     }
     newworld
 }
 
-fn displayworld(world: [[u8; 75]; 75])
-{
+fn displayworld(world: [[u8; 75]; 75]) {
     for i in 0..74 {
         for j in 0..74 {
-            if world[i][j] == 1 
-            {
+            if world[i][j] == 1 {
                 print!("{red}*", red = color::Fg(color::Red));
-            }
-            else 
-            {
+            } else {
                 print!(" ");
             }
         }
@@ -83,55 +89,53 @@ fn displayworld(world: [[u8; 75]; 75])
     }
 }
 
-fn census(world: [[u8; 75]; 75]) -> u16
-{
+fn census(world: [[u8; 75]; 75]) -> u16 {
     let mut count = 0;
 
     for i in 0..74 {
         for j in 0..74 {
-            if world[i][j] == 1
-            {
+            if world[i][j] == 1 {
                 count += 1;
             }
         }
     }
     count
 }
-fn generation(world: [[u8; 75]; 75]) -> [[u8; 75]; 75]
-{
+
+fn generation(world: [[u8; 75]; 75]) -> [[u8; 75]; 75] {
     let mut newworld = [[0u8; 75]; 75];
 
     for i in 0..74 {
         for j in 0..74 {
             let mut count = 0;
-            if i>0 {
-                count = count + world[i-1][j];
+            if i > 0 {
+                count = count + world[i - 1][j];
             }
-            if i>0 && j>0 {
-                count = count + world[i-1][j-1];
+            if i > 0 && j > 0 {
+                count = count + world[i - 1][j - 1];
             }
-            if i>0 && j<74 {
-                count = count + world[i-1][j+1];
+            if i > 0 && j < 74 {
+                count = count + world[i - 1][j + 1];
             }
-            if i<74 && j>0 {
-                count = count + world[i+1][j-1]
+            if i < 74 && j > 0 {
+                count = count + world[i + 1][j - 1]
             }
-            if i<74 {
-                count = count + world[i+1][j];
+            if i < 74 {
+                count = count + world[i + 1][j];
             }
-            if i<74 && j<74 {
-                count = count + world[i+1][j+1];
+            if i < 74 && j < 74 {
+                count = count + world[i + 1][j + 1];
             }
-            if j>0 {
-                count = count + world[i][j-1];
+            if j > 0 {
+                count = count + world[i][j - 1];
             }
-            if j<74 {
-                count = count + world[i][j+1];
+            if j < 74 {
+                count = count + world[i][j + 1];
             }
 
             newworld[i][j] = 0;
 
-            if (count <2) && (world[i][j] == 1) {
+            if (count < 2) && (world[i][j] == 1) {
                 newworld[i][j] = 0;
             }
             if world[i][j] == 1 && (count == 2 || count == 3) {
@@ -144,5 +148,3 @@ fn generation(world: [[u8; 75]; 75]) -> [[u8; 75]; 75]
     }
     newworld
 }
-
-
